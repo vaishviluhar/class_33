b833652c44ca57f35ab320f4afdaef974a8ddbe6
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2020-12-24 15:17:08 +0530

    Update Pig.js

--- Pig.js
+++ Pig.js
@@ -2,30 +2,25 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
-    this.Visiblity = 255;
+ //this.Visiblity=255;
   }
+display()
+{
+  //console.log(this.body.)
+if((this.body.speed)<2.5)
+{super.display();
+}else
+{
+World.remove(world,this.body)  
 
- display(){
-   //console.log(this.body.speed);
-   if(this.body.speed < 3){
-    super.display();
-   }
-   else{
-     World.remove(world, this.body);
-     push();
-     this.Visiblity = this.Visiblity - 5;
-     tint(255,this.Visiblity);
-     image(this.image, this.body.position.x, this.body.position.y, 50, 50);
-     pop();
-   }
-  }
+//push();
+//this.Visiblity=this.Visiblity-5;
+//tint(255,this.Visiblity)
 
-  score(){
-    if (this.Visiblity < 0 && this.Visiblity > -1005){
-      score++;
-    }
-  }
-
-
-
+//do nothing
+//image(this.image,this.body.position.x,this.body.position.y,50,50)
+//pop();
+}}
 };
+
+
